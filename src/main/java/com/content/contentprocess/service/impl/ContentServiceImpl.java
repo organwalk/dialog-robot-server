e2de38347a11a1d50ef8ffb42334212475784db2
haruki <hinzink@foxmail.com>
2023-06-28 21:25:12 +0800

    2023.06.28 亟需完善

--- src/main/java/com/content/contentprocess/service/impl/ContentServiceImpl.java
+++ src/main/java/com/content/contentprocess/service/impl/ContentServiceImpl.java
@@ -25,6 +25,7 @@ import java.util.stream.Collectors;
 
 public class ContentServiceImpl implements ContentService {
     private final GetDataListRedis getDataListRedis;
+    //
 
     @Override
     public OrderRespond getProcessResultByContent(OrderRequest orderRequest,String mobile) {
